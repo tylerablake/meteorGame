b339e3bde91d48e23fcf3fd309db3115fd457ba0
Tyler Blake <tyler.blake.louisville@gmail.com>
2016-03-28 17:48:59 -0400

    initial commit, v1 of game

---
+++ README.md
@@ -0,0 +1,9 @@
+# Building HTML5 Games: How to be a 2-figure Developer
+
+This repository is a step-by-step workshop for building a HTML5 video game with vanilla JavaScript.
+
+[The relevant slides are here.](http://ericlathrop.com/making-javascript-games/)
+
+The master branch contains only the assets, and the step branches progress through more features as the number gets higher.
+
+Made by [Two Scoop Games](http://twoscoopgames.com).
---
+++ main.js
@@ -0,0 +1,188 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+var keys = {
+	32 : "space",
+	37 : "left",
+	38 : "up",
+	39 : "right",
+	40 : "down",
+};
+var pressed = {};
+
+window.addEventListener("keydown", function(e){
+	pressed[keys[e.keyCode]] = true;
+});
+
+window.addEventListener("keyup", function(e){
+	pressed[keys[e.keyCode]] = false;
+});
+
+var animations = {
+	bullet:{
+		image : new Image(),
+		frame: 0,
+		frames : 5,
+		frameWidth : 55/5,
+		time: 0,
+		speed: 100
+	},
+	ship:{
+		image: new Image(),
+		frame : 0,
+		frames: 3,
+		frameWidth : 204/3,
+		time : 0,
+		speed: 200
+	},
+	meteor:{
+		image : new Image(),
+		frame : 0,
+		frames: 4,
+		frameWidth : 320/4,
+		time : 0,
+		speed: 300
+	}
+};
+animations.bullet.image.src = "images/bullet-f5.png";
+animations.ship.image.src = "images/ship-f3.png";
+animations.meteor.image.src = "images/meteor-f4.png";
+
+
+var explode = new Audio();
+explode.src = "sounds/explode.wav";
+explode.load();
+
+var laser = new Audio();
+laser.src = "sounds/laser.wav";
+laser.load();
+
+function playSound(sound){
+	var s = sound.cloneNode(true);
+	s.play();
+}
+
+
+function advanceAnimations(elapsed){
+	Object.keys(animations).forEach(function(name){
+		var anim = animations[name];
+		anim.time += elapsed;
+		while(anim.time > anim.speed){
+			anim.time -= anim.speed;
+			anim.frame++;
+			if(anim.frame >= anim.frames){
+				anim.frame = 0;
+			}
+		}
+		anim.x = anim.frame * anim.frameWidth;
+	});
+}
+
+function drawAnimation(context, name, x, y){
+	var anim = animations[name];
+	context.drawImage(anim.image, anim.x, 0, anim.frameWidth, anim.image.height, x , y, anim.frameWidth, anim.image.height);
+}
+
+var bullets= [];
+var meteors = [];
+
+var lastFrameTime = null;
+var fireTimerMax = 100;
+var fireTimer = fireTimerMax;
+
+var score = 0;
+
+var gameOver = false;
+
+function overlaps(x1,y1,w1,h1,x2,y2,w2,h2){
+	return x1 + w1 > x2 && x1 < x2 + w2 && y1 + h1 > y2 && y1 < y2 + h2;
+}
+
+var player = {
+	x : 350,
+	y : 500,
+	speed : .6
+};
+
+
+var render = function(time){
+	if(lastFrameTime === null){
+		lastFrameTime = time;
+	}
+	var elapsed = time - lastFrameTime;
+	lastFrameTime = time;
+
+		advanceAnimations(elapsed);
+		fireTimer += elapsed;
+
+		while(meteors.length < 1){
+			meteors.push({
+				x : (Math.floor(Math.random() * canvas.width - animations.meteor.frameWidth)),
+				y : -animations.meteor.image.height
+			});
+		}
+		if(!gameOver){
+			if(pressed["left"]){
+				player.x -= player.speed * elapsed;
+			}
+			if(pressed["right"]){
+				player.x += player.speed * elapsed;
+			}
+			if(pressed["up"]){
+				player.y -= player.speed * elapsed;
+			}
+			if(pressed["down"]){
+				player.y += player.speed * elapsed;
+			}
+			if(pressed["space"] && fireTimer > fireTimerMax){
+				fireTimer = 0;
+				playSound(laser);
+				bullets.push({x: player.x + (animations.ship.frameWidth / 2) - (animations.bullet.frameWidth / 2), y: player.y - animations.bullet.image.height
+				});
+			}
+		}
+	context.clearRect(0,0,canvas.width,canvas.height);
+
+	if(!gameOver){
+		drawAnimation(context, "ship", player.x, player.y);
+	}
+	for(var i = 0; i < bullets.length; i++){
+		var bullet = bullets[i];
+		drawAnimation(context,"bullet",bullet.x, bullet.y);
+		bullet.y -= 1 * elapsed;
+				for(var j = 0; j < meteors.length; j++){
+			var meteor = meteors[j];
+			if(!gameOver && overlaps(bullet.x, bullet.y, animations.bullet.frameWidth, animations.bullet.image.height,meteor.x, meteor.y, animations.meteor.frameWidth,animations.meteor.image.height)){
+				score++;
+				bullets.splice(i,1);
+				meteors.splice(j,1);
+				j++;
+				i--;
+			}
+		}
+	}
+	for(var i = 0; i < meteors.length; i++){
+		var meteor = meteors[i];
+		drawAnimation(context, "meteor", meteor.x, meteor.y);
+		meteor.y += 1 * elapsed;
+		if(meteor.y > canvas.height){
+			meteors.splice(i,1);
+			i--;
+			continue;
+		}
+		if(!gameOver && overlaps(player.x,player.y,animations.ship.frameWidth, animations.ship.image.height, meteor.x, meteor.y,animations.meteor.frameWidth, animations.meteor.image.height)){
+	gameOver = true;
+	playSound(explode);
+}
+	}
+	context.fillStyle = "#fff";
+	context.font = "25px helvetica";
+	context.fillText("Score: " + score, 50, 50);
+
+	if(gameOver){
+		context.fillStyle = "#fff";
+		context.fillText("GAME OVER!", 300, 300);
+	}
+	window.requestAnimationFrame(render);
+}
+window.requestAnimationFrame(render);
